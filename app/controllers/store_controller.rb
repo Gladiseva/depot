37efdb8eba4820ad07d4f6308731bf70400bb03e
lollijagladiseva <lollija.lukasova@gmail.com>
2021-01-22 16:10:31 +0100

    added cart functionality

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,12 @@
 class StoreController < ApplicationController
   def index
+    if session[:counter].nil?
+      session[:counter] = 0
+      puts session[:counter]
+    else
+      session[:counter] += 1
+      puts session[:counter]
+    end
     @products = Product.order(:title)
   end
 end
