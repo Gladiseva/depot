841d0d9876ac77d4968456cbb41ad2043e3443af
lollijagladiseva <lollija.lukasova@gmail.com>
2021-01-23 12:20:51 +0100

    added test two migrations

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "add already existing product to a cart" do
+    line_items_count_before_addition = line_items.length
+    cart = carts(:one)
+    cart.add_product(products(:one))
+
+    assert_equal line_items_count_before_addition, line_items.length
+  end
 end
