7effc1b60185a37622a8608fb722f951a11f2197
lollijagladiseva <lollija.lukasova@gmail.com>
2021-01-24 17:06:48 +0100

    modified tests fro Cart model

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,11 +1,24 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  test "add already existing product to a cart" do
-    line_items_count_before_addition = line_items.length
-    cart = carts(:one)
-    cart.add_product(products(:one))
+  def setup
+    @cart = Cart.create
+    @book_one = products(:ruby)
+    @book_two = products(:two)
+  end
 
-    assert_equal line_items_count_before_addition, line_items.length
+  test "add unique products" do
+    @cart.add_product(@book_one).save!
+    @cart.add_product(@book_two).save!
+    assert_equal 2, @cart.line_items.size
+    assert_equal @book_one.price + @book_two.price, @cart.total_price
+  end
+
+  test "add duplicate product" do
+    @cart.add_product(@book_one).save!
+    @cart.add_product(@book_one).save!
+    assert_equal 2 * @book_one.price, @cart.total_price
+    assert_equal 1, @cart.line_items.size
+    assert_equal 2, @cart.line_items[0].quantity
   end
 end
